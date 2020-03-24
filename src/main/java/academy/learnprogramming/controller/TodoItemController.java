45bfc992882b427ee224c19b6c99db5fb687653f
Vitalii Sednev <phen1007@gmail.com>
2020-03-24 20:58:57 +0400

    Добален исходных код

---
+++ src/main/java/academy/learnprogramming/controller/TodoItemController.java
@@ -0,0 +1,75 @@
+package academy.learnprogramming.controller;
+
+import academy.learnprogramming.model.TodoData;
+import academy.learnprogramming.model.TodoItem;
+import academy.learnprogramming.service.TodoItemService;
+import academy.learnprogramming.util.AttributeNames;
+import academy.learnprogramming.util.Mappings;
+import academy.learnprogramming.util.ViewNames;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+
+@Slf4j
+@Controller
+public class TodoItemController {
+
+    private final TodoItemService service;
+    // == constructor ==
+    @Autowired
+    public TodoItemController(TodoItemService service) {
+        this.service = service;
+    }
+
+    // == model attributes ==
+    @ModelAttribute
+    public TodoData todoData() {
+        return service.getData();
+    }
+
+    // == handler methods ==
+    // http://localhost:8080/todo-list/items
+    @GetMapping(Mappings.ITEMS)
+    public String items() {
+        return ViewNames.ITEMS_LIST;
+    }
+
+    @GetMapping(Mappings.ADD_ITEM)
+    public String addEditItem(@RequestParam(required = false, defaultValue = "-1") int id,
+                              Model model) {
+        log.info("editing id = {}", id);
+        TodoItem todoItem = service.getItem(id);
+
+        if(todoItem == null) {
+            todoItem = new TodoItem("", "", LocalDate.now());
+        }
+
+        model.addAttribute(AttributeNames.TODO_ITEM, todoItem);
+        return ViewNames.ADD_ITEM;
+    }
+
+    @PostMapping(Mappings.ADD_ITEM)
+    public String processItem(@ModelAttribute(AttributeNames.TODO_ITEM) TodoItem todoItem) {
+        log.info("todoItem from from = {}", todoItem);
+
+        if(todoItem.getId() == 0) {
+            service.addItem(todoItem);
+        } else {
+            service.updateItem(todoItem);
+        }
+        return "redirect:/" + Mappings.ITEMS;
+    }
+
+    @GetMapping(Mappings.DELETE_ITEM)
+    public String deleteItem(@RequestParam int id) {
+        log.info("Deleting item with id= {}", id);
+        service.removeItem(id);
+        return "redirect:/" + Mappings.ITEMS;
+    }
+
+
+}
---
+++ src/main/webapp/WEB-INF/view/welcome.jsp
@@ -0,0 +1,18 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Vitsed
+  Date: 23.03.2020
+  Time: 18:44
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Welcome</title>
+</head>
+<body>
+    <h1>${message}</h1>
+    <h2>age parameter is ${age}</h2>
+    <h2>${welcomeMessage}</h2>
+</body>
+</html>
